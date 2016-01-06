0d8d7e492c683da767211fdb4c22a1483c049582
Lindawu168 <lindawu16898@gmail.com>
2016-01-06 13:56:17 -0500

    LW add building&passed pipeline cucumber test

--- features/gocd_widge.feature
+++ features/gocd_widge.feature
@@ -3,17 +3,17 @@ Feature: display pipelines with gocd widge
 As a user, I would like to see status of build pipelines via gocd widge so that I will be notified when the status of the pipelines are changed.
 
 Scenario: Broken pipelines
- Given a broken gocd pipeline running on a gocd server
-   When I visit "/board/foo"
+    Given a broken gocd pipeline running on a gocd server
+     When I visit "/board/foo"
      Then I should see the widge of "foo" is broken
 
-      Scenario: Building a pipeline
-        Given a building gocd pipeline running on a gocd server
-            When I visit "/board/foo"
-                Then I should see the widge of "foo" is in process
+Scenario: Building a pipeline
+    Given a building gocd pipeline running on a gocd server
+     When I visit "/board/foo"
+     Then I should see the widge of "foo" is in process
 
 Scenario: Passed a pipeline
     Given a passed gocd pipeline running on a gocd server
-          When I visit "/board/foo"
-                Then I should see the widge of "foo" is passed
+     When I visit "/board/foo"
+     Then I should see the widge of "foo" is passed
 
--- features/step_definitions/gocd_widge.rb
+++ features/step_definitions/gocd_widge.rb
@@ -1,26 +1,26 @@
 Given(/^a broken gocd pipeline running on a gocd server$/) do
-   system 'ruby mb-stub.rb spec/mb-fixtures/gocd-foo-failed.json'
+     system 'ruby mb-stub.rb spec/mb-fixtures/gocd-foo-failed.json'
 end 
-   When(/^I visit "([^"]*)"$/) do |path|
-     visit path
-     end
-   Then(/^I should see the widge of "([^"]*)" is broken$/) do |title|
-      expect(page).to have_content(title);
-     expect(page).to have_selector('.gocd-stage.failed')
-   end
-
- Given(/^a building gocd pipeline running on a gocd server$/) do
-     system 'ruby mb-stub.rb spec/mb-fixtures/gocd-foo-in-progress.json'
-  end
-  Then(/^I should see the widge of "([^"]*)" is in process$/) do |title|
+When(/^I visit "([^"]*)"$/) do |path|
+      visit path
+end
+Then(/^I should see the widge of "([^"]*)" is broken$/) do |title|
      expect(page).to have_content(title);
+     expect(page).to have_selector('.gocd-stage.failed')
+end
+
+Given(/^a building gocd pipeline running on a gocd server$/) do
+      system 'ruby mb-stub.rb spec/mb-fixtures/gocd-foo-in-progress.json'
+end
+Then(/^I should see the widge of "([^"]*)" is in process$/) do |title|
+      expect(page).to have_content(title);
       expect(page).to have_selector('.gocd-stage.building')
-      end 
+end 
 
 Given(/^a passed gocd pipeline running on a gocd server$/) do
-   system 'ruby mb-stub.rb spec/mb-fixtures/gocd-foo-passed.json'
-     end
- Then(/^I should see the widge of "([^"]*)" is passed$/) do |title|
-    expect(page).to have_content(title);
-       expect(page).to have_selector('.gocd-stage.passed')
- end
+      system 'ruby mb-stub.rb spec/mb-fixtures/gocd-foo-passed.json'
+end
+Then(/^I should see the widge of "([^"]*)" is passed$/) do |title|
+      expect(page).to have_content(title);
+      expect(page).to have_selector('.gocd-stage.passed')
+end
