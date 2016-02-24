bc93d3a0ff705edf80e79a50c10e45bd9614ed41
Lindawu168 <lindawu16898@gmail.com>
2016-02-23 20:51:31 -0500

    Lw modified view_dashboard and support/env

--- features/step_definitions/view_dashboard.rb
+++ features/step_definitions/view_dashboard.rb
@@ -11,13 +11,13 @@ Given(/^a dashboard named "([^"]*)" page with widgets$/) do |board_name, table|
   table.hashes.each_with_index do |row, index|
     widget = case row[:type]
              when 'gocd_pipeline'
-               {'name' => row[:widget], 'pipeline' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost', 'pull_inteval' => 5, 'number_of_instances' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
+               {'name' => row[:widget], 'pipeline' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost:4545', 'pull_inteval' => 5, 'number_of_instances' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
              when 'jenkins_job'
-               {'name' => row[:widget], 'job' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost', 'pull_inteval' => 5, 'number_of_builds' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
+               {'name' => row[:widget], 'job' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost:4545', 'pull_inteval' => 5, 'number_of_builds' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
              when 'travis_ci'
-               {'name' => row[:widget], 'repo_path' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost', 'api_token' => '12345', 'pull_inteval' => 5, 'number_of_instances' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
+               {'name' => row[:widget], 'repo_path' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost:4545', 'api_token' => '12345', 'pull_inteval' => 5, 'number_of_instances' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
              else 
-               {'name' => row[:widget], 'pipeline' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost', 'pull_inteval' => 5, 'number_of_instances' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
+               {'name' => row[:widget], 'pipeline' => row[:widget], 'type' => row[:type], 'base_url' => 'http://localhost:4545', 'pull_inteval' => 5, 'number_of_instances' => 3, 'row' => 1, 'col' => index + 1, 'sizex' => 1, 'sizey' => 1}
              end
     widgets << widget
   end
